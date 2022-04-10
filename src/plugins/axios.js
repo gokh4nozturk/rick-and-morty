9ad0fe90af1d0e85fa699553424f11551e09cbc9
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-04-10 13:48:03 +0300

    feat: adds axios & configure axios

---
+++ src/plugins/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://rickandmortyapi.com/api/',
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+});
+
+export default instance;
