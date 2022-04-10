1ced1d955a9794ce4426c42454bbe177fe0225b2
Gökhan Öztürk <gokhaanozturk@gmail.com>
2022-04-10 13:29:38 +0300

    chore: remove css & adds tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
